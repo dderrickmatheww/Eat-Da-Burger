9885860f45f9f68a1580613f35f909745eb0c55f
Derrick <dderrickmatheww@gmail.com>
2019-05-27 12:14:17 -0400

    update on css,  update on pictures, update on js to call ajax to make post, put, and delete request

--- server.js
+++ server.js
@@ -1,21 +1,21 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var exphbs = require('express-handlerbars');
+var express = require("express");
+var bodyParser = require("body-parser");
+var exphbs = require("express-handlebars");
 
-var app = express()
+var app = express();
 var PORT = process.env.PORT || 3000;
 
 app.use(express.static("public"));
 
-app.use(bodyParser.urlencoded({ extended: true}))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-app.engine("handlebars", exphbs({ defaultLayout: "main"}));
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-var routes = require("./controllers/burgers_controller.js")
-app.use(routes)
+var routes = require("./controllers/burgers_controllers.js");
+app.use(routes);
 
-app.listen(PORT, function(){
-    console.log("Sever listening on: http://localhost:" + PORT);
-})
+app.listen(PORT, function() {
+  console.log("Server listening on: http://localhost:" + PORT);
+});
